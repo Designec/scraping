eef842f82718f913dd67ad7a825acb3b242d1f54
Aleksandr <c-alien@yandex.ru>
2019-09-07 23:17:51 +0500

    Update vk_get_photo_from_album.py

--- vk_get_photo_from_album.py
+++ vk_get_photo_from_album.py
@@ -10,8 +10,8 @@ count = 1000  # vk api за раз отдает не больше 1000
 offset = 0
 owner_id = -67785094
 album_id = '190843526'
-data_album = []  # сюда скинем все поулченные данные
-url_photos = []  # здесь будут ссылки на фото
+data_album = []
+url_photos = []
 
 response = requests.get('https://api.vk.com/method/photos.get',
                             params={'access_token': token, 'owner_id': owner_id,
@@ -31,7 +31,7 @@ while offset < photo_count:
                                     'offset': offset
                             })
     data_album.extend(response.json()['response']['items'])
-    offset += 1000  # смещаем
+    offset += 1000
 
 # перебираем спсок с данными, выдергиваем от туда ссылки на самые большие размеры фото
 # и скачиваем их в папку со скриптом
